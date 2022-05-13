ae073db8b8a28b26eed168eb6069686e24b73156
Igor Krivchun <igor@krivchun.ru>
2022-05-13 12:50:27 +0700

    Add debug logging and extend waiting time

--- authenticator.sh
+++ authenticator.sh
@@ -14,6 +14,8 @@ SUBDOMAIN=$(expr match "$CERTBOT_DOMAIN" '\(.*\)\..*\..*')
 # Create TXT record
 [[ -z "$SUBDOMAIN" ]] && CREATE_DOMAIN="_acme-challenge" || CREATE_DOMAIN="_acme-challenge.$SUBDOMAIN"
 
+echo "Creating domain at `date -R` for CERTBOT_DOMAIN: $CERTBOT_DOMAIN, DOMAIN: $DOMAIN, SUBDOMAIN: $SUBDOMAIN, CREATE_DOMAIN: $CREATE_DOMAIN"
+
 RECORD_ID=$(curl -s -X POST "https://pddimp.yandex.ru/api2/admin/dns/add" \
      -H "PddToken: $API_KEY" \
      -d "domain=$DOMAIN&type=TXT&content=$CERTBOT_VALIDATION&ttl=3600&subdomain=$CREATE_DOMAIN" \
@@ -26,18 +28,23 @@ fi
 
 echo $RECORD_ID > /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID
 
-# Sleep to make sure the change has time to propagate over to DNS
-#sleep 5
+echo "Created RECORD_ID: $RECORD_ID"
 
-# Sleep to make sure the change has time to propagate over to DNS (max: 30 min)
+# Sleep to make sure the change has time to propagate over to DNS
+sleep 60
+
+# Sleep to make sure the change has time to propagate over to DNS (180 min)
 c_time=0
-end_time=1800
+end_time=$[3600*3]
 while [ "$c_time" -le "$end_time" ]; do
         if [ `dig $CREATE_DOMAIN.$DOMAIN TXT +short @dns1.yandex.net | grep $CERTBOT_VALIDATION` ]; then
+		echo "@dns1.yandex.net is OK at `date -R`"
                 sleep 5
                 if [ `dig $CREATE_DOMAIN.$DOMAIN TXT +short @dns2.yandex.net | grep $CERTBOT_VALIDATION` ]; then
+			echo "@dns2.yandex.net is OK at `date -R`"
                         sleep 5
                         if [ `dig $CREATE_DOMAIN.$DOMAIN TXT +short @8.8.8.8 | grep $CERTBOT_VALIDATION` ]; then
+				echo "All dns is OK at `date -R`"
                                 sleep 5
                                 break
                         else
@@ -53,3 +60,9 @@ while [ "$c_time" -le "$end_time" ]; do
                 c_time=$[c_time+60]
         fi
 done
+
+left_time=$[end_time-c_time]
+if [ "$left_time" -ge 0 ]; then
+	echo "Seconds left: $left_time, sleeping"
+	sleep $left_time
+fi
